af2b0a183939876db6ea37466266cde623bbf560
Madhav Maheshwari1 <classwhitet@gmail.com>
2024-08-11 11:36:40 +0530

    Making Some Final Changes

--- Backend/server.js
+++ Backend/server.js
@@ -21,18 +21,18 @@ app.use('/api/message', messageRoutes);
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ Deployment + +-+-+-+-+-+-+-+-+-+-+-+-+
 
 const __dirname1 = path.resolve();
+console.log(__dirname1);
+
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    console.log("Serving static files from: ", path.join(__dirname1, "Backend", "frontend", "build"));
+    app.use(express.static(path.join(__dirname1, "/Backend/frontend/build")));
+    
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-    });
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running successfully");
+        console.log("Request received for: ", req.path);
+        res.sendFile(path.resolve(__dirname1, "Backend", "frontend", "build", "index.html"));
     });
 }
-
 
 // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ Deployment + +-+-+-+-+-+-+-+-+-+-+-+-+
 const PORT = process.env.PORT || 5000;
