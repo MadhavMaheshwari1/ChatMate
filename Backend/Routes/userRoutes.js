aded460a585838799e954523b92591f367ea082d
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-08-11 13:43:44 +0530

    Added Something

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser } = require("../Controllers/userController");
 const { authUser ,getAllUsers} = require("../Controllers/userController");
-const {protect} = require("../Middlwares/authMiddleWare");
+// const {protect} = require("../Middlwares/authMiddleWare");
 
 router.route('/').post(registerUser).get(protect,getAllUsers);
 router.post('/login', authUser);
