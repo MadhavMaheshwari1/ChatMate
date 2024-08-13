e2fc265c416910bb615366915fb60dd1b443f988
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-08-13 11:12:21 +0530

    Making some final Changes

--- Backend/frontend/src/App.js
+++ Backend/frontend/src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import ChatsPage from "./Pages/ChatsPage.jsx"
-import HomePage from "./Pages/HomePage.jsx"
+import React, { Suspense } from 'react';
+import { Spinner } from "@chakra-ui/react";
+
+// Lazy load the ChatsPage
+import HomePage from "./Pages/HomePage.jsx";
+const ChatsPage = React.lazy(() => import('./Pages/ChatsPage.jsx'));
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Chats" element={<ChatsPage />} />
+        <Route
+          path="/Chats"
+          element={
+            <Suspense fallback={<Spinner
+              size="xl"
+              w={20}
+              h={20}
+              margin="auto"
+            />}>
+              <ChatsPage />
+            </Suspense>
+          }
+        />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
